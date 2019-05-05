ba318253a8dcbc2fe91be0ff1bc6ea9d3a386ac0
misaka4e21 <misaka4e21@gmail.com>
2019-05-05 13:46:04 +0800

    Update hashie dependency to 3.6.0.

--- plugin.rb
+++ plugin.rb
@@ -7,7 +7,7 @@
 # GEMS
 gem 'domain_name', '0.5.20170404', { require: false }
 gem 'http-cookie', '1.0.3', { require: false }
-gem 'hashie', '3.5.5', { require: false }
+gem 'hashie', '3.6.0', { require: false }
 gem 'multi_json', '1.13.1', { require: false }
 gem 'net_dav', '0.5.1', { require: false }
 gem 'netrc', '0.8.0', { require: false }
